0c3e8ee85253054cb11b022c5cf72208408be017
MrsCenik <haticekubracenik@gmail.com>
2021-10-06 14:11:28 -0700

    mapsiz

--- src/Solution8.java
+++ src/Solution8.java
@@ -1,15 +1,8 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Solution8 {
-    /*
-    "Given an array of Strings, return the string with maximum size. If there is more than one strings in maximum size return all.
-        Example
-        [""also"",""dog"",""cat""] returns [""also""]
-        [""rotol"",""pol"",""puter""] returns [""rotol"",""puter""]"
-     */
+
     public static void main(String[] args) {
         String[] arr = {"also", "dog", "pol", "cat", "puter", "rotol"};
         maxLength(arr);
@@ -32,5 +25,4 @@ public class Solution8 {
         }
         System.out.println(longestElements);
     }
-
 }
