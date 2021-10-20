5586dcdef93df9e43b707cafff0f385b4e06b805
rmznkrc <68940507+rmznkrc@users.noreply.github.com>
2021-10-19 22:10:27 -0700

    Update Solution3.java

--- Solution3.java
+++ Solution3.java
@@ -1,1 +1,13 @@
-//starting
+public boolean checker(String a,char b){
+      
+        
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)==b){
+                return true;
+            }
+        }
+        return false;
+        
+
+
+        }
