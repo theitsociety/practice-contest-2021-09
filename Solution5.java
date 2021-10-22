a9ba0483ba687252a575cf02ff727b300e1ed613
rmznkrc <68940507+rmznkrc@users.noreply.github.com>
2021-10-21 18:57:51 -0700

    Update Solution5.java

--- Solution5.java
+++ Solution5.java
@@ -1,1 +1,19 @@
+    public static void main(String[] args) {
 
+
+        System.out.println(reverseString("Ali"));
+
+
+
+     }
+
+    public static String reverseString(String a){
+     String str =" ";
+     for(int i=a.length()-1;i>=0;i--){
+         str+=a.charAt(i);
+
+     }
+     return str;
+    }
+
+    }
