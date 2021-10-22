6eb0c5ccebf3b960ca25fd4fae8468bc38b6e8c7
rmznkrc <68940507+rmznkrc@users.noreply.github.com>
2021-10-21 23:56:07 -0700

    Update Solution10.java

--- Solution10.java
+++ Solution10.java
@@ -1,1 +1,12 @@
 
+
+    public static void  reverseArray() {
+
+        int [] arr=new int[]{1,2,3,4,5,6};
+
+        
+            for(int i=arr.length-1;i>=0;i--){
+                System.out.println(" Reverse array number: " + arr[i]);
+            }
+
+    }
