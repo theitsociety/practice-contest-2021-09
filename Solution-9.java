c87f07840ec118be6876bbc566d4c9abe88c589a
rmznkrc <68940507+rmznkrc@users.noreply.github.com>
2021-10-21 21:31:42 -0700

    Create Solution-9.java

---
+++ Solution-9.java
@@ -0,0 +1,13 @@
+ public static void oddNumber(){
+       int [] num=new int[]{2,3,4,5,6,76,7,23,45,7,98,13};
+        for(int i=0;i<num.length;i++){
+
+        if(num[i]%2!=0){
+            System.out.println("odd number : " + num[i]);
+        }else{
+            System.out.println("Even  numbers : " + num[i]);
+        }
+
+
+        }
+    }
