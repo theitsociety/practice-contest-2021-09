3e0030d368d5576e0b969b3857dd0b04acc0d057
rmznkrc <68940507+rmznkrc@users.noreply.github.com>
2021-10-25 08:13:33 -0700

    Update Solution10.java

--- Solution10.java
+++ Solution10.java
@@ -1,12 +1,15 @@
-
-
-    public static void  reverseArray() {
-
-        int [] arr=new int[]{1,2,3,4,5,6};
-
-        
-            for(int i=arr.length-1;i>=0;i--){
-                System.out.println(" Reverse array number: " + arr[i]);
+ public  static void reverseArray(int a[], int n) {
+            int[] b = new int[n];
+            int j = n;
+            for (int i = 0; i < n; i++) {
+                b[j - 1] = a[i];
+                j = j - 1;
             }
 
-    }
+            // printing the reversed array
+            System.out.println("Reversed array is: ");
+            for (int k = 0; k < n; k++) {
+                System.out.println(b[k]);
+            }
+
+        }
